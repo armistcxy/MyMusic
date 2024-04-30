cbde8e4ab2c3ffef63fc555a04158de46456e9cd
armistcxy <adlehoang118@gmail.com>
2024-04-30 23:54:44 +0700

    Add basic function for category service

--- api-server/app/schema/category.py
+++ api-server/app/schema/category.py
@@ -2,6 +2,14 @@ from pydantic import BaseModel
 import uuid
 
 
+class CategoryUploadForm(BaseModel):
+    name: str
+
+
+class CategoryUpdateForm(BaseModel):
+    name: str
+
+
 class CategoryResponse(BaseModel):
     id: uuid.UUID
     name: str
---
+++ api-server/app/service/category.py
@@ -0,0 +1,44 @@
+from app.model import models
+import app.repository.category as category_repo
+from app.repository.repo import get_session
+import app.schema.utils as schema_utils
+import uuid
+from app.schema.category import CategoryResponse, CategoryUploadForm, CategoryUpdateForm
+
+# create, get, update, delete category
+
+
+def insert_category(upload_form: CategoryUploadForm) -> CategoryResponse:
+    session = get_session()
+    category = models.Category(name=upload_form.name)
+    category = category_repo.insert_category(category=category)
+    response = schema_utils.category_model_to_response(category)
+    session.close()
+    return response
+
+
+def get_category_by_id(id: uuid.UUID) -> CategoryResponse:
+    session = get_session()
+    category = category_repo.get_category_by_id(id, session)
+    session.close()
+    return category
+
+
+def get_category_by_name(id: uuid.UUID) -> CategoryResponse:
+    session = get_session()
+    category = category_repo.get_category_by_name(id, session)
+    session.close()
+    return category
+
+
+def update_category(update_form: CategoryUpdateForm) -> CategoryResponse:
+    session = get_session()
+    # category = category_repo
+    session.close()
+    pass
+
+
+def delete_category_by_id(id: uuid.UUID):
+    session = get_session()
+    category_repo.delete_category(id)
+    session.close()
