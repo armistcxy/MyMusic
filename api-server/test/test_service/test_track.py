c5ef5a84f475ad1b4530a27dbcab131b770f7cde
armistcxy <adlehoang118@gmail.com>
2024-04-30 23:32:00 +0700

    Create mock upload form generation function

--- api-server/test/test_service/test_track.py
+++ api-server/test/test_service/test_track.py
@@ -4,7 +4,7 @@ from app.schema.track import TrackUploadForm
 from app.schema.category import CategoryResponse
 
 
-def test_upload_track():
+def mock_track_upload_form() -> TrackUploadForm:
     track_name = "kimi no namaiewasasdfdasesssss"
     track_length = 110
     artists_id = []
@@ -15,27 +15,22 @@ def test_upload_track():
         artists_id=artists_id,
         categories=categories,
     )
+    return track_form
+
+
+def test_upload_track():
+    track_form = mock_track_upload_form()
     track_response = track_service.upload_track(track_form=track_form)
 
     track_service.delete_track_by_id(id=track_response.id)
 
-    assert track_response.name == track_name
-    assert track_response.length == track_length
+    assert track_response.name == track_form.name
+    assert track_response.length == track_form.length
     # assert track_response.categories == CategoryResponse()
 
 
 def test_get_track_by_id():
-    track_name = "kimi no namasdfaiewdddasesssss"
-    track_length = 110
-    artists_id = []
-    categories = ["anime", "pop"]
-    track_form = TrackUploadForm(
-        name=track_name,
-        length=track_length,
-        artists_id=artists_id,
-        categories=categories,
-    )
-
+    track_form = mock_track_upload_form()
     track_response = track_service.upload_track(track_form=track_form)
 
     id = track_response.id
@@ -44,5 +39,5 @@ def test_get_track_by_id():
 
     track_service.delete_track_by_id(id)
 
-    assert track_response.name == track_name
-    assert track_length == 110
+    assert track_response.name == track_form.name
+    assert track_response.length == track_form.length
