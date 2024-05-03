8572549b28d26b3ea53d7a48eeb67f8774c599d7
22028196duckhanh <ngduckhanh1812@gmail.com>
2024-05-03 20:20:58 +0700

    construct layout

---
+++ front-end/src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Sidebar() {
+    return <Container>Sidebar</Container>
+}
+
+const Container = styled.div`
+    background-color: black;
+`;
