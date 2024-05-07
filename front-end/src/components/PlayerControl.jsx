3dd36b7b34e33b4cb02679f45c1d81653b7801be
22028196duckhanh <ngduckhanh1812@gmail.com>
2024-05-07 21:02:12 +0700

    add progressBar animation

--- front-end/src/components/PlayerControl.jsx
+++ front-end/src/components/PlayerControl.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import {
     BsFillPlayCircleFill,
@@ -13,20 +13,71 @@ import { reducerCases } from "../utils/Constants";
 import { Songs } from "./Songs";
 
 export default function PlayerControls() {
-    const [{ token, playerState }, dispatch] = useStateProvider();
+    const [{ token, playerState, currentPlaying }, dispatch] = useStateProvider();
+
+    const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrenttime] = useState(0);
+
+    const audioPlayer = useRef();
+    const progressBar = useRef(); 
+    const animationRef = useRef(); 
+
+    useEffect(() => {
+        const seconds = Math.floor(audioPlayer.current.duration);
+        setDuration(seconds);
+    
+        // set max prop with out seconds in input[range]
+        progressBar.current.max = seconds;
+      }, [audioPlayer?.current?.loadedmetada, audioPlayer?.current?.readyState]);
+
+      const whilePlaying = () => {
+        progressBar.current.value = audioPlayer.current.currentTime;
+        changeCurrentTime();
+        animationRef.current = requestAnimationFrame(whilePlaying);
+      };
+
+      const changeProgress = () => {
+        audioPlayer.current.currentTime = progressBar.current.value;
+        changeCurrentTime();
+      };
+    
+      const changeCurrentTime = () => {
+        progressBar.current.style.setProperty(
+          "--played-width",
+          `${(progressBar.current.value / duration) * 100}%`
+        );
+    
+        setCurrenttime(progressBar.current.value);
+      };
+
+      const calculateTime = (sec) => {
+        const minutes = Math.floor(sec / 60);
+        const returnMin = minutes < 10 ? `0${minutes}` : `${minutes}`;
+        const seconds = Math.floor(sec % 60);
+        const returnSec = seconds < 10 ? `0${seconds}` : `${seconds}`;
+        return `${returnMin}:${returnSec}`;
+      };
 
     const changeState = async () => {
-        const state = playerState ? "pause" : "play";
-        await axios.put(
-            `https://api.spotify.com/v1/me/player/${state}`,
-            {},
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: "Bearer " + token,
-                },
-            }
-        );
+        // const state = playerState ? "pause" : "play";
+        // await axios.put(
+        //     `https://api.spotify.com/v1/me/player/${state}`,
+        //     {},
+        //     {
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //             Authorization: "Bearer " + token,
+        //         },
+        //     }
+        // );
+        if (!playerState) {
+            audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying);
+          } else {
+            audioPlayer.current.pause();
+            cancelAnimationFrame(animationRef.current);
+          }
+
         dispatch({
             type: reducerCases.SET_PLAYER_STATE,
             playerState: !playerState,
@@ -46,7 +97,6 @@ export default function PlayerControls() {
                 },
             }
         );
-        console.log("changeTrack")
         dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true });
         const response = await axios.get(
             "https://api.spotify.com/v1/me/player/currently-playing",
@@ -74,6 +124,7 @@ export default function PlayerControls() {
 
     return (
         <Container>
+            <audio src={currentPlaying?.song} preload="metadata" ref={audioPlayer} />
             <Container1>
                 <div className="shuffle">
                     <BsShuffle />
@@ -96,14 +147,18 @@ export default function PlayerControls() {
                 </div>
             </Container1>
             <Container2 >
-                <div className="currentTime">00:00</div>
+                <div className="currentTime">{calculateTime(currentTime)}</div>
                 <input
                     type="range"
                     className="progressBar"
-
+                    ref = {progressBar}
+                    defaultValue="0"
+                    onChange={changeProgress}
                 />
                 <div className="duration" >
-
+                {duration && !isNaN(duration) && calculateTime(duration)
+              ? duration && !isNaN(duration) && calculateTime(duration)
+              : "00:00"}
                 </div>
             </Container2>
         </Container>
@@ -174,7 +229,7 @@ const Container2 = styled.div`
         top: 0;
         left: 0;
         background: #848484;
-        width: 50%;
+        width: var(--played-width);
         height: 100%;
         border-radius: 10px;
         z-index: 2;
