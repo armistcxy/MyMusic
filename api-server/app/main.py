014be50839c09eab5eec513d2502f695ddee0daa
Ngô Lê Hoàng <99264715+armistcxy@users.noreply.github.com>
2024-05-11 18:46:43 +0700

    Add cors

--- api-server/app/main.py
+++ api-server/app/main.py
@@ -6,6 +6,8 @@ from app.api.album import album_router
 from app.api.category import category_router
 from app.api.playlist import playlist_router
 from app.api.crawl import crawl_router
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 api = FastAPI()
 api.include_router(track_router)
@@ -15,6 +17,21 @@ api.include_router(album_router)
 api.include_router(category_router)
 api.include_router(playlist_router)
 api.include_router(crawl_router)
+
+api.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+origins = [
+    "http://localhost:8080",
+    "http://localhost",
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @api.get("/health")
